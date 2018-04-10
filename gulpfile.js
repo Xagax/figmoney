c735f07055223346872fd7f3ac2097d43475a948
Charlie Astrada <charlieastrada@xagax.com>
2018-04-10 16:05:06 -0300

    Add src folder to store the images, js, and scss files.
    * add imagemin and cleancss for gulp use
    * renamed main.min.css in index.html to main.css to facilitate the gulp sass task.
    * moved scss, js, and images to src folder

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,32 @@
 var gulp = require('gulp'); //gulp package
 var del = require('del'); //used for deleting public folder before running build
 var sass = require('gulp-sass'); //compile .scss files; used in sass task
+var cleanCSS = require('gulp-clean-css'); //to minify css files; used in sass task
+var imagemin = require('gulp-imagemin'); // optimize image files; used in imageMin task
+var browserSync = require('browser-sync').create(); //browserSync for change watching; used in watch task.
 
 
 //******* User Variables and Functions **********//
-
+function handleError (err) {
+  console.log(err.toString());
+  process.exit(-1);
+}
 
 //************* Gulp Tasks *************//
 
 //delete generated folder
 gulp.task('clean:public', function() {
   return del.sync('public');
+});
+
+//compile Sass and minify resulting css
+gulp.task('sass', function() {
+  gulp.src('src/scss/main.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(cleanCSS())
+      .on('error', handleError)
+      .pipe(gulp.dest('public/css/'))
+      .pipe(browserSync.reload({
+        stream: true
+      }));
 });
